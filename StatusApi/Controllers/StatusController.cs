adda0e989a8f9fc8b7c51e1551e55067c90511bd
Eva Mondragon <emondra1@progressive.com>
2021-11-15 11:09:53 -0500

    Add project files.

---
+++ StatusApi/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatusApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    //GET request http://localhost:5000/status
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        var response = new StatusResponse { 
+            Message = "The Server is great...Thanks", 
+            LastChecked = DateTime.Now};
+        return Ok(response);
+    }
+}
+
+
+public class StatusResponse
+{
+    public string? Message { get; set; }
+    public DateTime LastChecked { get; set; }
+}
